71953ccd973097d677899bececabe151df1ab611
Arkadiusz Kasprzak <Akasprzak016@gmail.com>
2021-12-16 13:38:03 +0100

    Add project files.

---
+++ UI/Startup.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using UI.Utils;
+
+namespace UI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddHttpClient("Default")
+                .ConfigurePrimaryHttpMessageHandler(() =>
+                {
+                    //TODO extract fileName and password to userSecrets
+                    var certificate = new X509Certificate2("/https/aspnetapp.pfx", "hard-password");
+                    var certificateValidator = new SingleCertificateValidator(certificate);
+
+                    return new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback = certificateValidator.Validate
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ UI/Utils/SingleCertificateValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UI.Utils
+{
+    public class SingleCertificateValidator
+    {
+        private readonly X509Certificate2 _trustedCertificate;
+
+        public SingleCertificateValidator(X509Certificate2 trustedCertificate)
+        {
+            _trustedCertificate = trustedCertificate;
+        }
+
+        public bool Validate(HttpRequestMessage httpRequestMessage, X509Certificate2 x509Certificate2, X509Chain x509Chain, SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            var thumbprint = x509Certificate2.GetCertHashString();
+            return thumbprint == _trustedCertificate.Thumbprint;
+        }
+    }
+}
